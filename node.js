97a5237b0c5232043ccc4d2ddfdf37863b810ed1
huntingbug1 <huntingbug1@gmail.com>
2026-06-05 14:46:14 +0400

    Initialize package.json for dummy Node.js project

---
+++ node.js
@@ -0,0 +1,10 @@
+{
+  "name": "dummy-project",
+  "version": "1.0.0",
+  "description": "Dummy Node.js project",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "license": "MIT"
+}
